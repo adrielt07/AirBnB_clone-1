3da24c120830924dd9eebfe65a52170d6b1abf73
Adriel Tolentino <adriel_tolentino@outlook.com>
2018-07-30 22:53:33 +0000

    fix pep8

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,11 +3,13 @@ from fabric.api import put, run, local
 import os
 import time
 
+
 def do_pack():
     timestr = time.strftime("%Y%m%d%H%M%S")
     if os.path.exists('./versions') is False:
         local("mkdir versions")
-    execute = "tar -cvzf ./versions/web_static_{}.tgz web_static".format(timestr)
+    execute = "tar -cvzf ./versions/web_static_{}.tgz web_static".format(
+        timestr)
     result = local(execute)
     if result.succeeded:
         return "versions/web_static_{}".format(timestr)
