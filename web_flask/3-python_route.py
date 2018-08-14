643c98d72fd8c06725080fdb24e27e7454015dfd
adrielt07 <adrieltolentino@outlook.com>
2018-08-14 02:19:34 +0000

    initial commit

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strick_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is(text):
+    return 'c {}'.format(text)
+
+
+@app.route('/python/<text>', defaults={'text': None})
+@app.route('/python/<text>')
+def python_is(text):
+    if text is None:
+        return 'Python is cool'
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
